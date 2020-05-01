0afec913ef2650b89b828efbaa001b6aad4b34c8
Franco Bacinello <francobacinello22@hotmail.com>
2020-05-01 21:30:13 +0200

    algo

--- Inicio.py
+++ Inicio.py
@@ -4,6 +4,21 @@ Created on Fri May  1 12:44:10 2020
 
 @author: Franco
 """
+import csv, sys
 
-print("Hola")
-print("Chau")
+def process(d,n,p):
+    sys.stdout.write("\nFecha: " + d + " Nombre: " + n + " Precio: " + str(p))
+
+print("Comenzando proceso:")
+
+
+with open('archivo_csv_columnas.txt', 'r') as f:
+    reader = csv.DictReader(f, delimiter=':')
+    for row in reader:
+        date = row["date"]
+        nombre = row["nombre"]
+        closing_price = float(row["closing_price"])
+        process(date, nombre, closing_price)
+
+print("\n\nProceso finalizado")
+        
